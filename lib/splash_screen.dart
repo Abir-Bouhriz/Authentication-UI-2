6f428d4cf95ef80a78d03cf5e454428c4d185d2f
Abir-Bouhriz <abirbouhrizdaidj@gmail.com>
2022-04-07 13:27:01 +0200

    create splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import './login.dart';
+
+class SplashScreen extends StatefulWidget {
+  // const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  _navigateToLogin() async {
+    await Future.delayed(Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Login(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E319D),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset(
+                'assets/images/logo.png',
+                height: 150,
+                width: 150,
+              ),
+            ),
+            Container(
+              child: Text(
+                'JetLight Studio',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
